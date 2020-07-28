a887294bca46d709da882b18919dc7cbcd9807e8
vitor00almei <pondebatata@outlook.com>
2020-07-28 01:44:21 -0300

    msm8953-common: update scripts for Paranoid
    * also update copyright
    
    Change-Id: I922d337924c51adec8f2e9a9a9202f686c73c1a2

--- init/init_msm8953.cpp
+++ init/init_msm8953.cpp
@@ -1,6 +1,7 @@
 /*
    Copyright (c) 2016, The CyanogenMod Project
    Copyright (C) 2019 The LineageOS Project.
+   Copyright (C) 2020 Paranoid Android
 
    Redistribution and use in source and binary forms, with or without
    modification, are permitted provided that the following conditions are
